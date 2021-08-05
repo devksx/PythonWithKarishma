0952ad68caa19489cf312a8d4498d28acab04a99
Karishma Sukhwani <42887239+Karishma0210@users.noreply.github.com>
2021-08-05 17:34:08 +0400

    set up and variables

---
+++ lesson1.py
@@ -0,0 +1,40 @@
+print('Hello')
+print('lessson1')
+
+x = "tree" #x is a variable and it can hold any value
+# "=" this sign is called assignment sign in pythhon
+
+print("what is the value of x? Ans.=>" + x)
+y = "Tina"
+print("what is the name of y?", y)
+print(x + y) #concatenation
+a="Boy"
+b = "girl"
+print(a + b) 
+
+'''
+Numbers = 1, 2 -1, 0, 1.4, -6.7
+	integer - int (example: -1, 0, 3),
+	float = 1.0, 1.8, -1.9, 0.0
+String: "child", 'x'
+List: list  = ["karishma", "jimmy", "orange"]
+Tuple
+Dictionary
+'''
+
+myVar = 1
+print("Tell me the type", type(myVar))
+
+myVar = 1.0
+print("Tell me the type", type(myVar))
+
+myVar = "joseph"
+print("Tell me the type", type(myVar))
+
+myVar = [ "Karishma", "is", "teaching", 1, 5, 0, 1.7]
+print("what is the first item in my list",  myVar[0])
+print("what is the third item in my list",  myVar[2])
+print("length of my list", len(myVar))
+print("what is last item in my list?", myVar[len(myVar)-1], myVar[-1])
+print("last second item in the list", myVar[-2])
+print("Tell me the type", type(myVar))
